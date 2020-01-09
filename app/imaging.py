5876d4c49e3bf48c92d8c2b39ff8366bb7c2d43b
j-penson <jpenson24@gmail.com>
2020-01-09 22:51:45 +0000

    refactor: split array subtraction and image conversion

--- app/imaging.py
+++ app/imaging.py
@@ -19,7 +19,8 @@ def read_image(image_path: str = './images/logo.png') -> np.array:
     :return: m*n*3 numpy array representing the logo
     """
     im = imageio.imread(image_path, pilmode='RGB')
-    return np.array(im)
+    im_array = np.array(im)
+    return im_array
 
 
 def get_distortion_array(m: int = 200, n: int = 200, distortion_amount: int = 3) -> np.array:
@@ -31,21 +32,29 @@ def get_distortion_array(m: int = 200, n: int = 200, distortion_amount: int = 3)
     :return: m*n*3 numpy array for distortion on half the image
     """
     half_width = int(n / 2)
-    array_rand = np.random.randint(low=0, high=distortion_amount, size=(m, half_width, 3), dtype=np.uint8)
+    array_rand = np.random.randint(low=0, high=distortion_amount+1, size=(m, half_width, 3), dtype=np.uint8)
     array_zeros = np.zeros((m, half_width, 3), dtype=np.uint8)
     return np.concatenate((array_rand, array_zeros), axis=1)
 
 
-def distort_image(image_array: np.array, distortion_array: np.array) -> BytesIO:
-    """Combine the image and distortion arrays, check all values are positive and return the image.
+def combine_distort_array(image_array: np.array, distortion_array: np.array) -> np.array:
+    """Combine the image and distortion arrays, convert all values to a positive value and return the array.
 
     :param image_array: m*n*3 numpy array of original image
     :param distortion_array: m*n*3 distortion array
-    :return:
+    :return: array of distorted image
     """
-    # TODO split out into distort function and return function
-    array_out = image_array - distortion_array
-    array_out[array_out < 0] = 0
-    img_bytes = imageio.imwrite('<bytes>', im=array_out, format='PNG')
+    img_dist_array = image_array - distortion_array
+    img_dist_array[img_dist_array < 0] = 0
+    return img_dist_array
+
+
+def convert_distorted_image(dist_img_out: np.array) -> BytesIO:
+    """Convert the distorted array to a image for serving
+
+    :param dist_img_out: array of distorted image
+    :return: BytesIO stream of image
+    """
+    img_bytes = imageio.imwrite('<bytes>', im=dist_img_out, format='PNG')
     img = BytesIO(img_bytes)
     return img
